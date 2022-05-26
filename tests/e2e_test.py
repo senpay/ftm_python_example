fc215f6d6d4bf041566ed8c661e4f24dad7e4d95
Alex Pushkarev <alexander.pushkarev@codurance.com>
2022-05-26 15:11:21 +0100

    refactor a bit

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -9,13 +9,6 @@ URL = 'http://localhost:5000'
 @ddt.ddt
 class TestStringMethods(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get(URL)
-        self.driver.maximize_window()
-
-    def tearDown(self):
-        self.driver.close()
 
     @ddt.data(
             (50, '1£'),
@@ -32,8 +25,32 @@ class TestStringMethods(unittest.TestCase):
     )
     def test_before_one_hour(self, case):
         duration, payment = case
-        duration_field = self.driver.find_element_by_id("duration_input")
-        duration_field.send_keys(duration)
-        self.driver.find_element_by_id("submit_button").click()
-        result = self.driver.find_element_by_id("amount")
-        self.assertEqual(result.text, f"You have to pay: {payment}")
+
+        self.type_into_input("duration_input", duration)
+        self.click_on_the_element("submit_button")
+        result = self.get_element_text("amount")
+
+        self.assertEqual(result, f"You have to pay: {payment}")
+
+
+
+
+
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get(URL)
+        self.driver.maximize_window()
+
+    def tearDown(self):
+        self.driver.close()
+
+    def type_into_input(self, locator, text):
+        duration_field = self.driver.find_element_by_id()
+        duration_field.send_keys(text)
+    
+    def click_on_the_element(self, locator):
+        self.driver.find_element_by_id(locator).click()
+
+    def get_element_text(self, locator):
+        return self.driver.find_element_by_id(locator).text
